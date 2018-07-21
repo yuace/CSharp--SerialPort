0e67df3f97c492bb8a11cf873ea0a7045a347f06
zhangyuace <zhangyuace@foxmail.com>
2018-07-21 16:24:44 +0800

    MySerialPort

---
+++ MySerialPort/MySerialPort/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace MySerialPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbBaud = new System.Windows.Forms.ComboBox();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ReceiveTbox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SendTbox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.cmbBaud);
+            this.groupBox1.Controls.Add(this.cmbPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口设置";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbBaud
+            // 
+            this.cmbBaud.FormattingEnabled = true;
+            this.cmbBaud.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "115200"});
+            this.cmbBaud.Location = new System.Drawing.Point(100, 86);
+            this.cmbBaud.Name = "cmbBaud";
+            this.cmbBaud.Size = new System.Drawing.Size(90, 20);
+            this.cmbBaud.TabIndex = 3;
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(100, 26);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(90, 20);
+            this.cmbPort.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "波特率：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "串 口：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ReceiveTbox);
+            this.groupBox2.Location = new System.Drawing.Point(30, 159);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(364, 125);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "接收数据";
+            // 
+            // ReceiveTbox
+            // 
+            this.ReceiveTbox.Location = new System.Drawing.Point(45, 32);
+            this.ReceiveTbox.Multiline = true;
+            this.ReceiveTbox.Name = "ReceiveTbox";
+            this.ReceiveTbox.ReadOnly = true;
+            this.ReceiveTbox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.ReceiveTbox.Size = new System.Drawing.Size(251, 75);
+            this.ReceiveTbox.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.SendTbox);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(30, 312);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(364, 127);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "发送数据";
+            // 
+            // SendTbox
+            // 
+            this.SendTbox.Location = new System.Drawing.Point(45, 20);
+            this.SendTbox.Multiline = true;
+            this.SendTbox.Name = "SendTbox";
+            this.SendTbox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.SendTbox.Size = new System.Drawing.Size(203, 75);
+            this.SendTbox.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 460);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbBaud;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox SendTbox;
+        private System.Windows.Forms.TextBox ReceiveTbox;
+    }
+}
+
---
+++ MySerialPort/MySerialPort/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySerialPort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;//设置该属性 为false
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey keyCom = Registry.LocalMachine.OpenSubKey("Hardware\\DeviceMap\\SerialComm");
+            if (keyCom != null)
+            {
+                string[] sSubKeys = keyCom.GetValueNames();
+                cmbPort.Items.Clear();
+                foreach (string sName in sSubKeys)
+                {
+                    string sValue = (string)keyCom.GetValue(sName);
+                    cmbPort.Items.Add(sValue);
+                }
+                if (cmbPort.Items.Count > 0)
+                    cmbPort.SelectedIndex = 0;
+            }
+          
+                cmbBaud.Text = "115200";
+
+        }
+        
+        bool isOpened = false;//串口状态标志
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isOpened)
+            {
+                serialPort.PortName = cmbPort.Text;
+                serialPort.BaudRate = Convert.ToInt32(cmbBaud.Text, 10);
+                try
+                {
+                    serialPort.Open();     //打开串口
+                    button1.Text = "关闭串口";
+                    cmbPort.Enabled = false;//关闭使能
+                    cmbBaud.Enabled = false;
+                    isOpened = true;
+                    serialPort.DataReceived += new SerialDataReceivedEventHandler(post_DataReceived);//串口接收处理函数
+                }
+                catch
+                {
+                    MessageBox.Show("串口打开失败！");
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort.Close();     //关闭串口
+                    button1.Text = "打开串口";
+                    cmbPort.Enabled = true;//打开使能
+                    cmbBaud.Enabled = true;
+                    isOpened = false;
+                }
+                catch
+                {
+                    MessageBox.Show("串口关闭失败！");
+                }
+            }
+            
+        }
+        private void post_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string str = serialPort.ReadExisting();//字符串方式读
+            ReceiveTbox.Text = "";//先清除上一次的数据
+            ReceiveTbox.Text += str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //发送数据
+            if (serialPort.IsOpen)
+            {//如果串口开启
+                if (SendTbox.Text.Trim() != "")//如果框内不为空则
+                {
+                    serialPort.Write(SendTbox.Text.Trim());//写数据
+                }
+                else
+                {
+                    MessageBox.Show("发送框没有数据");
+                }
+            }
+            else
+            {
+                MessageBox.Show("串口未打开");
+            }
+        }
+    }
+}
